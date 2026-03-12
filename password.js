e6412607eab83a01238364dd611dfa20793e37d1
Jovan Filipov <jovan.filipov05@gmail.com>
2026-03-12 19:13:43 +0100

    Add .gitignore and stop tracking sensitive files

--- password.js
+++
@@ -1,9 +0,0 @@
-import bcrypt from "bcrypt";
-import pool from "./src/db/pool.js";
-
-const hash = await bcrypt.hash("password123", 10);
-
-await pool.query(`
-  UPDATE users
-  SET password_hash = $1
-`, [hash]);
